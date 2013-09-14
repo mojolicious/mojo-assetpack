ffff3dd985abfc479aa443507118544f8121165c
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-09-14 13:48:39 +0200

    Improved documentation

--- lib/Mojolicious/Plugin/Compress.pm
+++ lib/Mojolicious/Plugin/Compress.pm
@@ -10,17 +10,52 @@ Mojolicious::Plugin::Compress - Compress css, scss and javascript with external 
 
 =head1 DESCRIPTION
 
+In production mode:
+
 This plugin will automatically compress scss, less, css and javascript with
 the help of external application. The result will be one file with all the
 sources combined.
+
+This is done using L</compress_javascripts> and L</compress_stylesheets>.
+
+In development mode:
+
+This plugin will expand the input files to multiple javascript / link tags
+which makes debugging easier.
+
+This is done using L</expand_files>.
 
 =head1 APPLICATIONS
 
 =head2 less
 
+LESS extends CSS with dynamic behavior such as variables, mixins, operations
+and functions. See L<http://lesscss.org> for more details.
+
+Installation on Ubuntu and Debian:
+
+  $ sudo apt-get install npm
+  $ sudo npm install -g less
+
 =head2 sass
 
+Sass makes CSS fun again. Sass is an extension of CSS3, adding nested rules,
+variables, mixins, selector inheritance, and more. See L<http://sass-lang.com>
+for more information.
+
+Installation on Ubuntu and Debian:
+
+  $ sudo apt-get install rubygems
+  $ sudo gem install sass
+
 =head2 yuicompressor
+
+L<http://yui.github.io/yuicompressor> is used to compress javascript and css.
+
+Installation on Ubuntu and Debian:
+
+  $ sudo apt-get install npm
+  $ sudo npm -g i yuicompressor
 
 =head1 SYNOPSIS
 
@@ -38,6 +73,8 @@ In your template:
 NOTE! You need to have one line for each type, meaning you cannot combine
 javascript and css sources on one line.
 
+See also L</register>.
+
 =cut
 
 use Mojo::Base 'Mojolicious::Plugin';
@@ -49,6 +86,11 @@ use constant DEBUG => $ENV{MOJO_COMPRESS_DEBUG} || 0;
 
 our $VERSION = '0.01';
 our %APPLICATIONS; # should consider internal usage, may change without warning
+
+sub _system {
+  warn "@_" if DEBUG;
+  system @_;
+}
 
 =head1 ATTRIBUTES
 
@@ -62,9 +104,138 @@ has out_dir => '';
 
 =head1 METHODS
 
+=head2 compress_javascripts
+
+  $bytestream = $self->compress_javascripts($c, @rel_files);
+
+This method will compress the input files to a file in the L</out_dir>
+with the name of the MD5 sum of the C<@files>.
+
+Will also run L</yuicompressor> on the input files to minify them.
+
+The returning bytestream will contain a javascript tag.
+
+=cut
+
+sub compress_javascripts {
+  my($self, $c, @files) = @_;
+  my $static = $c->app->static;
+  my $file = md5_sum(join '', @files) .'.js';
+  my $out = catfile $self->out_dir, $file;
+  my $tmp = catfile tmpdir(), $file;
+
+  unless(-e $out) {
+    open my $OUT, '>', $out or die "Write $out: $!";
+
+    for my $file (@files) {
+      my $in = $static->file($file)->path;
+      _system $APPLICATIONS{js} => $in => -o => $tmp;
+      print $OUT slurp $tmp if -e $tmp;
+    }
+
+    unlink $tmp if -e $tmp;
+  }
+
+  return $c->javascript($self->_abs_to_rel($c, $out));
+}
+
+=head2 compress_stylesheets
+
+  $bytestream = $self->compress_stylesheets($c, @rel_files);
+
+This method will compress the input files to a file in the L</out_dir>
+with the name of the MD5 sum of the C<@files>.
+
+Will also run L</less> or L</sass> on the input files to minify them.
+
+The returning bytestream will contain a style tag.
+
+=cut
+
+sub compress_stylesheets {
+  my($self, $c, @files) = @_;
+  my $static = $c->app->static;
+  my $file = md5_sum(join '', @files) .'.css';
+  my $out = catfile $self->out_dir, $file;
+  my $tmp = catfile tmpdir(), $file;
+
+  unless(-e $out) {
+    open my $OUT, '>', $out or die "Write $out: $!";
+
+    for my $file (@files) {
+      $file =~ /\.(scss|less)$/ or next;
+      my $in = $static->file($file)->path;
+      _system $APPLICATIONS{$1} => $in => $tmp;
+      print $OUT slurp $tmp if -e $tmp;
+    }
+
+    unlink $tmp if -e $tmp;
+  }
+
+  return $c->stylesheet($self->_abs_to_rel($c, $out));
+}
+
+=head2 expand_files
+
+  $bytestream = $self->expand_files($c, @rel_files);
+
+This method will return one tag pr. input file which holds the uncompressed
+version of the sources.
+
+Will also run L</less> or L</sass> on the input files to convert them to
+css, which the browser understand.
+
+The returning bytestream will contain style or javascript tags.
+
+=cut
+
+sub expand_files {
+  my($self, $c, @files) = @_;
+  my $type = $files[0] =~ /\.(js|css|scss|less)$/ ? $1 : '';
+
+  if($type eq 'js') {
+    return b join '', map { $c->javascript($_) } @files;
+  }
+  else {
+    return b join '', map { $c->stylesheet($self->_convert_file($c, $_)) } @files;
+  }
+}
+
+=head2 find_external_apps
+
+This method is used to find the L</APPLICATIONS>. It will look for the apps
+usin L<File::Which> in this order: lessc, less, sass, yui-compressor,
+yuicompressor.
+
+=cut
+
+sub find_external_apps {
+  my($self, $app, $config) = @_;
+
+  $APPLICATIONS{less} = $config->{less} || which('lessc') || which('less');
+  $APPLICATIONS{scss} = $config->{sass} || which('sass');
+  $APPLICATIONS{js} = $config->{yuicompressor} || which('yui-compressor') || which('yuicompressor');
+
+  for(keys %APPLICATIONS) {
+    $APPLICATIONS{$_} and next;
+    $app->log->warn("Could not find application for $_");
+  }
+}
+
 =head2 register
 
-Will register the C<compress> helper.
+  plugin 'Compress', {
+    enable => $bool,
+    out_dir => '/abs/path/to/app/public/dir',
+    less => '/path/to/lessc',
+    sass => '/path/to/sass',
+    js => '/path/to/yuicompressor',
+  };
+
+Will register the C<compress> helper. All arguments are optional.
+
+"enable" will default to COMPRESS_ASSETS environment variable or set to true
+if L<Mojolicious/mode> is "production".
 
 =cut
 
@@ -72,17 +243,27 @@ sub register {
   my($self, $app, $config) = @_;
   my $enable = $config->{enable} // $ENV{COMPRESS_ASSETS} // $app->mode eq 'production';
 
-  $self->_find_external_apps($app, $config);
+  $self->find_external_apps($app, $config);
   $self->out_dir($config->{out_dir} || $app->home->rel_dir('public/compressed'));
 
   mkdir $self->out_dir; # TODO: Use mkpath instead?
 
   if($enable) {
-    $app->helper(compress => sub { $self->_compress_files(@_) });
+    $app->helper(compress => sub { 
+      my($c, @files) = @_;
+      my $type = $files[0] =~ /\.(js|css|scss|less)$/ ? $1 : '';
+
+      if($type eq 'js') {
+        return $self->compress_javascripts($c, @files);
+      }
+      else {
+        return $self->compress_stylesheets($c, @files);
+      }
+    });
   }
   else {
     $app->log->debug('Mojolicious::Plugin::Compress will expand file list');
-    $app->helper(compress => sub { $self->_expand_files(@_) });
+    $app->helper(compress => sub { $self->expand_files(@_) });
   }
 }
 
@@ -96,76 +277,6 @@ sub _abs_to_rel {
   die "$out is not found in static paths";
 }
 
-sub _find_external_apps {
-  my($self, $app, $config) = @_;
-
-  $APPLICATIONS{less} = $config->{less} || which('lessc') || which('less');
-  $APPLICATIONS{scss} = $config->{sass} || which('sass');
-  $APPLICATIONS{js} = $config->{yuicompressor} || which('yui-compressor') || which('yuicompressor');
-
-  for(keys %APPLICATIONS) {
-    $APPLICATIONS{$_} and next;
-    $app->log->warn("Could not find application for $_");
-  }
-}
-
-sub _compress_files {
-  my($self, $c, @files) = @_;
-  my $type = $files[0] =~ /\.(js|css|scss|less)$/ ? $1 : '';
-
-  if($type eq 'js') {
-    return $self->_compress_javascripts($c, @files);
-  }
-  else {
-    return $self->_compress_stylesheets($c, @files);
-  }
-}
-
-sub _compress_javascripts {
-  my($self, $c, @files) = @_;
-  my $static = $c->app->static;
-  my $file = md5_sum(join '', @files) .'.js';
-  my $out = catfile $self->out_dir, $file;
-  my $tmp = catfile tmpdir(), $file;
-
-  unless(-e $out) {
-    open my $OUT, '>', $out or die "Write $out: $!";
-
-    for my $file (@files) {
-      my $in = $static->file($file)->path;
-      system $APPLICATIONS{js} => $in => -o => $tmp;
-      print $OUT slurp $tmp if -e $tmp;
-    }
-
-    unlink $tmp if -e $tmp;
-  }
-
-  return $c->javascript($self->_abs_to_rel($c, $out));
-}
-
-sub _compress_stylesheets {
-  my($self, $c, @files) = @_;
-  my $static = $c->app->static;
-  my $file = md5_sum(join '', @files) .'.css';
-  my $out = catfile $self->out_dir, $file;
-  my $tmp = catfile tmpdir(), $file;
-
-  unless(-e $out) {
-    open my $OUT, '>', $out or die "Write $out: $!";
-
-    for my $file (@files) {
-      $file =~ /\.(scss|less)$/ or next;
-      my $in = $static->file($file)->path;
-      system $APPLICATIONS{$1} => $in => $tmp;
-      print $OUT slurp $tmp if -e $tmp;
-    }
-
-    unlink $tmp if -e $tmp;
-  }
-
-  return $c->stylesheet($self->_abs_to_rel($c, $out));
-}
-
 sub _convert_file {
   my($self, $c, $file) = @_;
 
@@ -173,8 +284,7 @@ sub _convert_file {
     eval {
       my $in = $c->app->static->file($file)->path;
       (my $out = $in) =~ s/\.(\w+)$/.css/;
-      warn "$APPLICATIONS{$1} $in $out" if DEBUG;
-      system $APPLICATIONS{$1} => $in => $out;
+      _system $APPLICATIONS{$1} => $in => $out;
       $file =~ s/\.(\w+)$/.css/;
     } or do {
       $c->app->log->warn("Could not convert $file: $@");
@@ -182,18 +292,6 @@ sub _convert_file {
   }
 
   $file;
-}
-
-sub _expand_files {
-  my($self, $c, @files) = @_;
-  my $type = $files[0] =~ /\.(js|css|scss|less)$/ ? $1 : '';
-
-  if($type eq 'js') {
-    return b join '', map { $c->javascript($_) } @files;
-  }
-  else {
-    return b join '', map { $c->stylesheet($self->_convert_file($c, $_)) } @files;
-  }
 }
 
 =head1 AUTHOR
