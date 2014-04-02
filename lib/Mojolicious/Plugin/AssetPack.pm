2fa3992ac0263331262a6e6b5bfd2651fa8d1ef7
Alexander Rymasheusky <deadhh@gmail.com>
2014-04-02 19:05:37 +0300

    Added a reprocess option.

--- lib/Mojolicious/Plugin/AssetPack.pm
+++ lib/Mojolicious/Plugin/AssetPack.pm
@@ -66,9 +66,8 @@ makes debugging and development easier.
 
 This is done using L</expand>.
 
-TIP! Make morbo watch your less/sass files as well:
-
-  $ morbo -w lib -w templates -w public/sass
+TIP! Set L</reprocess> to true to convert your less/sass/coffee files each time
+they're requested.
 
 =head2 Preprocessors
 
@@ -110,6 +109,11 @@ our %MISSING_ERROR = (
 
 Set this to true if the assets should be minified.
 
+=head2 reprocess
+
+Set this to true to reprocess the assets each time they're expanded (useful
+for development).  Cannot be used with minify.
+
 =head2 preprocessors
 
 Holds a L<Mojolicious::Plugin::AssetPack::Preprocessors> object.
@@ -123,6 +127,7 @@ unless a L<static directory|Mojolicious::Static/paths> is writeable.
 =cut
 
 has minify => 0;
+has reprocess => 0;
 has preprocessors => sub { Mojolicious::Plugin::AssetPack::Preprocessors->new };
 has out_dir => sub { File::Spec::Functions::catdir(File::Spec::Functions::tmpdir(), 'mojo-assetpack') };
 
@@ -160,27 +165,33 @@ sub add {
   if($self->minify) {
     $self->process($moniker => @files);
   }
-  else {
-    my %extensions = (
-      less => 'css',
-      sass => 'css',
-      scss => 'css',
-      coffee => 'js',
-    );
-    for my $file (@files) {
-      my ($extension) = $file =~ /\.(\w+)$/;
-      next unless exists $extensions{$extension};
-      my $target_ext = $extensions{$extension};
-
-      my $moniker = basename $file;
-      $moniker =~ s/\.\w+$/.$target_ext/;
-      $self->process($moniker => $file);
-      $file = delete $self->{assets}{$moniker};
-    }
-    $self->{assets}{$moniker} = \@files;
+  elsif(!$self->reprocess) {
+    my @processed_files = $self->_process_many($moniker, @files);
+    $self->{assets}{$moniker} = \@processed_files;
   }
 
   $self;
+}
+
+sub _process_many {
+  my($self, $moniker, @files) = @_;
+  my %extensions = (
+    less => 'css',
+    sass => 'css',
+    scss => 'css',
+    coffee => 'js',
+  );
+  for my $file (@files) {
+    my ($extension) = $file =~ /\.(\w+)$/;
+    next unless exists $extensions{$extension};
+    my $target_ext = $extensions{$extension};
+
+    my $moniker = basename $file;
+    $moniker =~ s/\.\w+$/.$target_ext/;
+    $self->process($moniker => $file);
+    $file = delete $self->{assets}{$moniker};
+  }
+  return @files;
 }
 
 =head2 expand
@@ -190,7 +201,9 @@ sub add {
 This method will return one tag for each asset defined by the "$moniker".
 
 Will also run L</less>, L</sass> or L</coffee> on the files to convert them to
-css or js, which the browser understands.
+css or js, which the browser understands. (With L</reprocess> enabled, this is
+done each time on expand; with it disabled, this is done single time when the
+asset is added.)
 
 The returning bytestream will contain style or script tags.
 
@@ -203,11 +216,20 @@ sub expand {
   if(!ref $files) {
     return b "<!-- Cannot expand $moniker -->";
   }
-  elsif($moniker =~ /\.js/) {
-    return b join "\n", map { $c->javascript($_) } @$files;
+
+  my @processed_files;
+  if ($self->reprocess) {
+    @processed_files = $self->_process_many($moniker, @$files);
   }
   else {
-    return b join "\n", map { $c->stylesheet($_) } @$files;
+    @processed_files = @$files;
+  }
+
+  if($moniker =~ /\.js/) {
+    return b join "\n", map { $c->javascript($_) } @processed_files;
+  }
+  else {
+    return b join "\n", map { $c->stylesheet($_) } @processed_files;
   }
 }
 
@@ -300,8 +322,12 @@ sub register {
   my($self, $app, $config) = @_;
   my $minify = $config->{minify} // $app->mode eq 'production';
   my $helper = $config->{helper} || 'asset';
+  my $reprocess = $config->{reprocess} // 0;
+
+  die "Cannot use reprocess with minify" if $minify && $reprocess;
 
   $self->minify($minify);
+  $self->reprocess($reprocess);
   $self->preprocessors->detect unless $config->{no_autodetect};
 
   $self->{assets} = {};
