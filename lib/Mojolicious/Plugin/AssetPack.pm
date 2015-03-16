0e52bf24eb94fdc67d49e6eabf0b4531f3534f93
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-03-16 13:47:19 +0100

    Released version 0.41
           * Will die on CSS::Sass errors

--- lib/Mojolicious/Plugin/AssetPack.pm
+++ lib/Mojolicious/Plugin/AssetPack.pm
@@ -10,7 +10,7 @@ use File::Spec ();
 use constant CACHE_ASSETS => $ENV{MOJO_ASSETPACK_NO_CACHE} ? 0 : 1;
 use constant DEBUG => $ENV{MOJO_ASSETPACK_DEBUG} || 0;
 
-our $VERSION = '0.40';
+our $VERSION = '0.41';
 
 has base_url      => '/packed/';
 has fallback      => 0;
@@ -330,7 +330,7 @@ Mojolicious::Plugin::AssetPack - Compress and convert css, less, sass, javascrip
 
 =head1 VERSION
 
-0.40
+0.41
 
 =head1 SYNOPSIS
 
