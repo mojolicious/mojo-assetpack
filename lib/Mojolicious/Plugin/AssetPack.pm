ecb6648c5ee425677f65f65402ebb62cfbda85cb
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-08-09 14:28:10 +0200

    Released version 0.58
     - Change "source_paths" to return original reference and not a copy
     - Need to look for packed files in all directories (source_paths+public)

--- lib/Mojolicious/Plugin/AssetPack.pm
+++ lib/Mojolicious/Plugin/AssetPack.pm
@@ -12,7 +12,7 @@ use File::Spec     ();
 use constant DEBUG    => $ENV{MOJO_ASSETPACK_DEBUG}    || 0;
 use constant NO_CACHE => $ENV{MOJO_ASSETPACK_NO_CACHE} || 0;
 
-our $VERSION = '0.57';
+our $VERSION = '0.58';
 
 has base_url      => '/packed/';
 has minify        => 0;
@@ -405,7 +405,7 @@ Mojolicious::Plugin::AssetPack - Compress and convert css, less, sass, javascrip
 
 =head1 VERSION
 
-0.57
+0.58
 
 =head1 SYNOPSIS
 
