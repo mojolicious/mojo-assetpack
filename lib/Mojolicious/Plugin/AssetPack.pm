f14a63a45ff0fd0c277c396ae94e94f8ae9a4a4e
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-08-20 17:21:57 +0200

    Released version 0.60
     - Fix detecting changed sass/scss files in sub directories, with leading underscore

--- lib/Mojolicious/Plugin/AssetPack.pm
+++ lib/Mojolicious/Plugin/AssetPack.pm
@@ -12,7 +12,7 @@ use File::Spec     ();
 use constant DEBUG    => $ENV{MOJO_ASSETPACK_DEBUG}    || 0;
 use constant NO_CACHE => $ENV{MOJO_ASSETPACK_NO_CACHE} || 0;
 
-our $VERSION = '0.59';
+our $VERSION = '0.60';
 
 has base_url      => '/packed/';
 has minify        => 0;
@@ -403,7 +403,7 @@ Mojolicious::Plugin::AssetPack - Compress and convert css, less, sass, javascrip
 
 =head1 VERSION
 
-0.59
+0.60
 
 =head1 SYNOPSIS
 
