f4c75e421cc8ad137e1d399c1d7613985abdcfde
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-08-23 13:34:41 +0200

    Released version 0.61
     - Add include_paths() to Scss preprocessor
     - Fix using SASS_PATH environment variable when calculating checksum #60
     - Deprecating asset->preprocessor() in favor of asset->preprocessors->add()

--- lib/Mojolicious/Plugin/AssetPack.pm
+++ lib/Mojolicious/Plugin/AssetPack.pm
@@ -12,7 +12,7 @@ use File::Spec     ();
 use constant DEBUG    => $ENV{MOJO_ASSETPACK_DEBUG}    || 0;
 use constant NO_CACHE => $ENV{MOJO_ASSETPACK_NO_CACHE} || 0;
 
-our $VERSION = '0.60';
+our $VERSION = '0.61';
 
 has base_url      => '/packed/';
 has minify        => 0;
@@ -393,7 +393,7 @@ Mojolicious::Plugin::AssetPack - Compress and convert css, less, sass, javascrip
 
 =head1 VERSION
 
-0.60
+0.61
 
 =head1 SYNOPSIS
 
