19dfc2377340c0b9f7696484a882d5401a5d4229
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-01-13 09:36:43 +0100

    Will recreate assets on change

--- lib/Mojolicious/Plugin/Assetpipe/Store.pm
+++ lib/Mojolicious/Plugin/Assetpipe/Store.pm
@@ -1,19 +1,24 @@
 package Mojolicious::Plugin::Assetpipe::Store;
 use Mojo::Base 'Mojolicious::Static';
-use Mojo::Util 'spurt';
+use Mojo::JSON;
+use Mojo::Util qw(slurp spurt);
 use Mojolicious::Plugin::Assetpipe::Util qw(diag checksum DEBUG);
 use File::Basename 'dirname';
+
+# MOJO_ASSETPIPE_DB_FILE is used in tests
+use constant DB_FILE => $ENV{MOJO_ASSETPIPE_DB_FILE} || 'assetpipe.db';
 
 # TODO: Remove access to private attribute $asset->_asset()
 
 sub load {
-  my ($self, $asset, $args) = @_;
-  my @rel = $self->_cache_path($asset, $args || {});
+  my ($self, $asset, $attr) = @_;
+  my @rel = $self->_cache_path($asset, $attr || {});
   my $file = $self->file(join '/', @rel);
 
-  diag 'Load "%s" = %s', eval { $file->path } || join('/', @rel), $file ? 1 : 0 if DEBUG;
   return 0 unless $file;
-  $asset->$_($args->{$_}) for keys %$args;
+  return 0 unless $self->_db($asset, $attr)->{checksum} eq $asset->checksum;
+  diag 'Load "%s" = 1', eval { $file->path } || join('/', @rel) if DEBUG;
+  $asset->$_($attr->{$_}) for keys %$attr;
   $asset->_asset($file);
 }
 
@@ -26,19 +31,41 @@ sub save {
   mkdir $dir if !-d $dir and -w dirname $dir;
   diag 'Save "%s" = %s', $path, -d $dir ? 1 : 0 if DEBUG;
   return 0 unless -w $dir;
+  $self->_db($asset, {}, 1);
   spurt $asset->content, $path;
   return $asset->_asset(Mojo::Asset::File->new(path => $path));
 }
 
 sub _cache_path {
-  my ($self, $asset, $args) = @_;
+  my ($self, $asset, $attr) = @_;
   return (
     'processed', sprintf '%s-%s.%s%s',
     $asset->name,
     checksum($asset->url),
-    $args->{minified} || $asset->minified ? 'min.' : '',
+    $attr->{minified} || $asset->minified ? 'min.' : '',
     $asset->format
   );
+}
+
+sub _db {
+  my ($self, $asset, $attr, $save) = @_;
+  my $db_file = File::Spec->catfile($self->paths->[0], DB_FILE);
+
+  my $db = $self->{_db} ||= do {
+    -r $db_file ? Mojo::JSON::decode_json(slurp $db_file) : {};
+  };
+
+  my $data = $db->{$asset->url} ||= {};
+  my $key = sprintf 'minified:%s', $attr->{minified} // $asset->minified ? 1 : 0;
+  $data = $data->{$key} ||= {checksum => '', format => '', mtime => 0};
+
+  if ($save) {
+    $data->{$_} = $asset->$_ for qw(checksum format mtime);
+    diag 'Save "%s" = %s', $db_file, -w $self->paths->[0] ? 1 : 0 if DEBUG;
+    spurt Mojo::JSON::encode_json($db), $db_file if -w $self->paths->[0];
+  }
+
+  return $data;
 }
 
 1;
