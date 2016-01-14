f1a401a22ff6e6ca3b7197f91985cb46a80f4360
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-01-14 13:25:35 +0100

    Store understands Mojo::Asset, not Mojolicious::Plugin::Assetpipe::Asset

--- lib/Mojolicious/Plugin/Assetpipe/Store.pm
+++ lib/Mojolicious/Plugin/Assetpipe/Store.pm
@@ -8,62 +8,56 @@ use File::Basename 'dirname';
 # MOJO_ASSETPIPE_DB_FILE is used in tests
 use constant DB_FILE => $ENV{MOJO_ASSETPIPE_DB_FILE} || 'assetpipe.db';
 
-# TODO: Remove access to private attribute $asset->_asset()
-
 sub load {
-  my ($self, $asset, $attr) = @_;
-  my @rel = $self->_cache_path($asset, $attr || {});
+  my ($self, $attr) = @_;
+  my @rel = $self->_cache_path($attr);
   my $file = $self->file(join '/', @rel);
 
   return 0 unless $file;
-  return 0
-    unless $self->_db($asset, $attr)->{checksum} eq
-    ($attr->{checksum} || $asset->checksum);
+  return 0 unless $self->_db($attr)->{checksum} eq $attr->{checksum};
   diag 'Load "%s" = 1', eval { $file->path } || join('/', @rel) if DEBUG;
-  $asset->$_($attr->{$_}) for keys %$attr;
-  $asset->_asset($file);
+  return $file;
 }
 
 sub save {
-  my ($self, $asset, $attr) = @_;
-  my $path = File::Spec->catfile($self->paths->[0], $self->_cache_path($asset, $attr));
+  my ($self, $ref, $attr) = @_;
+  my $path = File::Spec->catfile($self->paths->[0], $self->_cache_path($attr));
   my $dir = dirname $path;
 
   # Do not care if this fail. Can fallback to temp files.
   mkdir $dir if !-d $dir and -w dirname $dir;
   diag 'Save "%s" = %s', $path, -d $dir ? 1 : 0 if DEBUG;
-  return 0 unless -w $dir;
-  $asset->$_($attr->{$_}) for keys %$attr;
-  $self->_db($asset, {}, 1);
-  spurt $asset->content, $path;
-  return $asset->_asset(Mojo::Asset::File->new(path => $path));
+  return Mojo::Asset::Memory->new->content($$ref) unless -w $dir;
+  $self->_db($attr, 1);
+  spurt $$ref, $path;
+  return Mojo::Asset::File->new(path => $path);
 }
 
 sub _cache_path {
-  my ($self, $asset, $attr) = @_;
+  my ($self, $attr) = @_;
   return (
-    'processed', sprintf '%s-%s.%s%s',
-    $asset->name,
-    checksum($asset->url),
-    $attr->{minified} || $asset->minified ? 'min.' : '',
-    $attr->{format} || $asset->format
+    'cache', sprintf '%s-%s.%s%s',
+    $attr->{name},
+    checksum($attr->{url}),
+    $attr->{minified} ? 'min.' : '',
+    $attr->{format}
   );
 }
 
 sub _db {
-  my ($self, $asset, $attr, $save) = @_;
+  my ($self, $attr, $save) = @_;
   my $db_file = File::Spec->catfile($self->paths->[0], DB_FILE);
 
   my $db = $self->{_db} ||= do {
     -r $db_file ? Mojo::JSON::decode_json(slurp $db_file) : {};
   };
 
-  my $data = $db->{$asset->url} ||= {};
-  my $key = sprintf 'minified:%s', $attr->{minified} // $asset->minified ? 1 : 0;
+  my $data = $db->{$attr->{url}} ||= {};
+  my $key = sprintf 'minified:%s', $attr->{minified} ? 1 : 0;
   $data = $data->{$key} ||= {checksum => '', format => '', mtime => 0};
 
   if ($save) {
-    $data->{$_} = $asset->$_ for qw(checksum format mtime);
+    $data->{$_} = $attr->{$_} for qw(checksum format mtime);
     diag 'Save "%s" = %s', $db_file, -w $self->paths->[0] ? 1 : 0 if DEBUG;
     spurt Mojo::JSON::encode_json($db), $db_file if -w $self->paths->[0];
   }
