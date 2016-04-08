2ade04b900e82be8950074367e6b45bc632ce7a6
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-04-08 20:15:35 +0200

    Released version 1.03
     - Add Pipe::Reloader for instant refresh in browser on asset change #72
     - Marked old modules as deprecated

--- lib/Mojolicious/Plugin/AssetPack.pm
+++ lib/Mojolicious/Plugin/AssetPack.pm
@@ -4,7 +4,7 @@ use Mojolicious::Plugin::AssetPack::Asset::Null;
 use Mojolicious::Plugin::AssetPack::Store;
 use Mojolicious::Plugin::AssetPack::Util qw(diag has_ro load_module DEBUG);
 
-our $VERSION = '1.02';
+our $VERSION = '1.03';
 
 has minify => sub { shift->_app->mode ne 'development' };
 
@@ -201,7 +201,7 @@ Mojolicious::Plugin::AssetPack - Compress and convert css, less, sass, javascrip
 
 =head1 VERSION
 
-1.02
+1.03
 
 =head1 SYNOPSIS
 
