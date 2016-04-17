ca42a268bdbceca069dee6ff0e0e2e95b18439fb
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-04-17 14:22:12 +0200

    Released version 1.06
     - Fix Fetch with Combine #75

--- lib/Mojolicious/Plugin/AssetPack.pm
+++ lib/Mojolicious/Plugin/AssetPack.pm
@@ -4,7 +4,7 @@ use Mojolicious::Plugin::AssetPack::Asset::Null;
 use Mojolicious::Plugin::AssetPack::Store;
 use Mojolicious::Plugin::AssetPack::Util qw(diag has_ro load_module DEBUG);
 
-our $VERSION = '1.05';
+our $VERSION = '1.06';
 
 has minify => sub { shift->_app->mode eq 'development' ? 0 : 1 };
 
@@ -216,7 +216,7 @@ Mojolicious::Plugin::AssetPack - Compress and convert css, less, sass, javascrip
 
 =head1 VERSION
 
-1.05
+1.06
 
 =head1 SYNOPSIS
 
