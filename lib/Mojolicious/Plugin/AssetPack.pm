7788622b501fa640b7a8e475de8f4e379de043e8
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-09-11 17:02:00 +0200

    Released version 1.23
     - Add EXPERIMENTAL support for mixing in dynamic SASS files

--- lib/Mojolicious/Plugin/AssetPack.pm
+++ lib/Mojolicious/Plugin/AssetPack.pm
@@ -6,7 +6,7 @@ use Mojolicious::Plugin::AssetPack::Asset::Null;
 use Mojolicious::Plugin::AssetPack::Store;
 use Mojolicious::Plugin::AssetPack::Util qw(diag has_ro load_module DEBUG);
 
-our $VERSION = '1.22';
+our $VERSION = '1.23';
 
 my %TAG_TEMPLATE;
 $TAG_TEMPLATE{css} = [qw(link rel stylesheet href)];
@@ -248,7 +248,7 @@ Mojolicious::Plugin::AssetPack - Compress and convert css, less, sass, javascrip
 
 =head1 VERSION
 
-1.22
+1.23
 
 =head1 SYNOPSIS
 
