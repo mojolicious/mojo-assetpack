0f4ae72bdb7cef4eb57f732a3a0cb13db3f9bc9c
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-01-23 14:12:35 +0100

    Released version 1.38
     - Fix missing path() function in Pipe::Sass

--- lib/Mojolicious/Plugin/AssetPack.pm
+++ lib/Mojolicious/Plugin/AssetPack.pm
@@ -6,7 +6,7 @@ use Mojolicious::Plugin::AssetPack::Asset::Null;
 use Mojolicious::Plugin::AssetPack::Store;
 use Mojolicious::Plugin::AssetPack::Util qw(diag has_ro load_module DEBUG);
 
-our $VERSION = '1.37';
+our $VERSION = '1.38';
 
 my %TAG_TEMPLATE;
 $TAG_TEMPLATE{css} = [qw(link rel stylesheet href)];
@@ -277,7 +277,7 @@ Mojolicious::Plugin::AssetPack - Compress and convert css, less, sass, javascrip
 
 =head1 VERSION
 
-1.37
+1.38
 
 =head1 SYNOPSIS
 
