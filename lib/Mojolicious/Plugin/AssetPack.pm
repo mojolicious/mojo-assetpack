4da155de0707005a884088ea16aa4b579cfb0355
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-09-27 21:35:59 +0200

    Released version 1.46
     - Add "asset_class" attribute to AssetPack::Store
     - Add rollupjs pipe - https://rollupjs.org/

--- lib/Mojolicious/Plugin/AssetPack.pm
+++ lib/Mojolicious/Plugin/AssetPack.pm
@@ -6,7 +6,7 @@ use Mojolicious::Plugin::AssetPack::Asset::Null;
 use Mojolicious::Plugin::AssetPack::Store;
 use Mojolicious::Plugin::AssetPack::Util qw(diag has_ro load_module DEBUG);
 
-our $VERSION = '1.45';
+our $VERSION = '1.46';
 
 my %TAG_TEMPLATE;
 $TAG_TEMPLATE{css} = [qw(link rel stylesheet href)];
@@ -254,7 +254,7 @@ Mojolicious::Plugin::AssetPack - Compress and convert css, less, sass, javascrip
 
 =head1 VERSION
 
-1.45
+1.46
 
 =head1 SYNOPSIS
 
