9b409547d78d444261fdf1ce5b65d7ec607a17fc
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-10-29 10:21:02 +0800

    Released version 1.48
     - Fix RollupJs package name #130
     - load_module() confess on error
     - Removed EXPERIMENTAL Reloader pipe
     - Removed EXPERIMENTAL clone() method in AssetPack::Asset

--- lib/Mojolicious/Plugin/AssetPack.pm
+++ lib/Mojolicious/Plugin/AssetPack.pm
@@ -6,7 +6,7 @@ use Mojolicious::Plugin::AssetPack::Asset::Null;
 use Mojolicious::Plugin::AssetPack::Store;
 use Mojolicious::Plugin::AssetPack::Util qw(diag has_ro load_module DEBUG);
 
-our $VERSION = '2.00';
+our $VERSION = '1.48';
 
 my %TAG_TEMPLATE;
 $TAG_TEMPLATE{css} = [qw(link rel stylesheet href)];
@@ -252,7 +252,7 @@ Mojolicious::Plugin::AssetPack - Compress and convert css, less, sass, javascrip
 
 =head1 VERSION
 
-2.00
+1.48
 
 =head1 SYNOPSIS
 
